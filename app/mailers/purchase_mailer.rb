93e9df0b2f589e15078d9d8f5529fdcf96e371c2
Diego Raggio <didiraggio@gmail.com>
2017-02-25 22:57:49 -0300

    added mailer template

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,11 @@
+class PurchaseMailer < ActionMailer::Base
+	layout 'purchase_mailer'
+	default from: "Learn rails <learnappcode@gmail.com>"
+
+	def purchase_receipt purchase
+		@purchase = purchase
+		mail to: purchase.email,
+		subject: "Thank you  for you buisness!"
+		
+	end
+end
